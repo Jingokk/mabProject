a7d6b96de4b1fb9465265d126caafc522a7afbc2
jingokk <anujinaa1205@gmail.com>
2023-11-27 17:15:48 +0800

    second

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import socket from "./socket";
 
 const App = () => {
   const [index, setIndex] = react.useState([]);
+  const [inputIndex, setInputIndex] = react.useState(0);
+  const myRefs = react.useRef([]);
 
   useEffect(() => {
     socket.on("initialTable", (table) => {
@@ -24,44 +26,59 @@ const App = () => {
 
   const indexHandler = (i) => {
     socket.emit("index", i);
+    setInputIndex(i);
   };
 
   return (
     <VStack
       bg={"#ddd"}
-      w={"100vw"}
-      h={"100vh"}
+      w={"full"}
+      minH={"100vh"}
       align={"center"}
       justify={"center"}
+      spacing={0}
+      overflowY={"auto"}
+      overflowX={"hidden"}
+      py={"10"}
+      px={["10", "10", "16", "44", "44"]}
     >
       {index.length > 0
         ? index.map((e, i) => (
             <Input
+              ref={(el) => (myRefs.current[i] = el)}
               key={e.index}
               bg={"#fff"}
-              w={"sm"}
+              w={"full"}
+              rounded={"none"}
               value={e.text}
+              maxLength={200}
+              border={"none"}
               onChange={(e) => inputHandler(e.target.value, i)}
               onKeyDown={(e) => {
                 if (e.key === "Enter") {
                   indexHandler(i + 1);
+                  if (myRefs.current[i + 1]) {
+                    myRefs.current[i + 1].focus();
+                  }
                 }
+                if (e.key === "ArrowUp") {
+                  if (myRefs.current[i - 1]) {
+                    myRefs.current[i - 1].focus();
+                  }
+                }
+                if (e.key === "ArrowDown") {
+                  if (myRefs.current[i + 1]) {
+                    myRefs.current[i + 1].focus();
+                  }
+                }
+              }}
+              autoFocus={inputIndex === i ? true : false}
+              _focusVisible={{
+                outline: "none",
               }}
             />
           ))
         : null}
-      {/* <Text>TEXT: {data}</Text>
-      <Textarea
-        w={"lg"}
-        rounded={"md"}
-        outline={"none"}
-        minH={"md"}
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        _focusVisible={{
-          outline: "none",
-        }}
-      /> */}
     </VStack>
   );
 };
--- socketIo/initiateSocket.js
+++ socketIo/initiateSocket.js
@@ -1,6 +1,8 @@
 const { Server } = require("socket.io");
 
 var io;
+let connectedClients = 0;
+let serverTimeOut;
 
 const initiateSocket = (server) => {
   io = new Server(server);
@@ -12,11 +14,21 @@ const initiateSocket = (server) => {
     { index: 2, text: "" },
     { index: 3, text: "" },
     { index: 4, text: "" },
+    { index: 5, text: "" },
+    { index: 6, text: "" },
+    { index: 7, text: "" },
+    { index: 8, text: "" },
+    { index: 9, text: "" },
   ];
 
   io.on("connection", (socket) => {
     console.log("client connected", socket.id);
     socket.join("room");
+    connectedClients++;
+
+    if (connectedClients === 1) {
+      restartServerTimeOut();
+    }
 
     socket.emit("initialTable", table);
 
@@ -53,8 +65,37 @@ const initiateSocket = (server) => {
 
     socket.on("disconnect", () => {
       console.log("client disconnected", socket.id);
+      connectedClients--;
+
+      if (connectedClients === 0) {
+        restartServerTimeOut();
+      }
     });
   });
+
+  const restartServerTimeOut = () => {
+    const SERVER_RESTART_TIMEOUT = 1000 * 60 * 5;
+
+    if (serverTimeOut) {
+      clearTimeout(serverTimeOut);
+    }
+
+    serverTimeOut = setTimeout(() => {
+      console.log("No clients connected. Restarting the server.");
+      table = [
+        { index: 0, text: "" },
+        { index: 1, text: "" },
+        { index: 2, text: "" },
+        { index: 3, text: "" },
+        { index: 4, text: "" },
+        { index: 5, text: "" },
+        { index: 6, text: "" },
+        { index: 7, text: "" },
+        { index: 8, text: "" },
+        { index: 9, text: "" },
+      ];
+    }, SERVER_RESTART_TIMEOUT);
+  };
 };
 
 module.exports = { initiateSocket };
